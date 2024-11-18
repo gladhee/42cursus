5dbf0d7e8b57dae482ca4279d44523c650d90f34
heechoi <heechoi@student.42seoul.kr>
2024-11-18 15:12:04 +0900

    fix convention

--- CPP_Module/CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module/CPP_Module_08/ex00/easyfind.hpp
@@ -9,9 +9,9 @@
 #include <exception>
 #include <stdexcept>
 
-template <typename T>
+template<typename T>
 typename T::iterator easyfind(T &container, int value) {
-	typename T::iterator it =  std::find(container.begin(), container.end(), value);
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
 
 	if (it == container.end()) {
 		throw std::invalid_argument("Value not found.");
