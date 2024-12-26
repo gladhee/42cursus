b025b4ad49ae78332c768e77df057bff70a8536a
heechoi <heechoi@student.42seoul.kr>
2024-12-26 16:37:22 +0900

    cpp 07 fix

--- CPP_Module/CPP_Module_07/ex01/iter.hpp
+++ CPP_Module/CPP_Module_07/ex01/iter.hpp
@@ -15,13 +15,6 @@ void iter(T *array, size_t length, void (*func)(T const &)) {
 }
 
 template<typename T>
-void iter(T const *array, size_t length, void (*func)(T const &)) {
-	for (size_t i = 0; i < length; i++) {
-		func(array[i]);
-	}
-}
-
-template<typename T>
 void print(T const &value) {
 	std::cout << value << " ";
 }
